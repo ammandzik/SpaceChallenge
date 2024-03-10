b31b31b5e1e8068b065b6c781d1b079143e603b6
ammandzik <preisner.amanda@gmail.com>
2024-03-10 13:59:44 +0100

    Initial commit - overall project plan

---
+++ src/Simulation.java
@@ -0,0 +1,26 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.io.File;
+
+public class Simulation {
+
+    public static void loadItems () {
+
+    }
+
+    public static void loadU1 (){
+
+    }
+
+    public static void loadU2 () {
+
+    }
+
+    public static void runSimulation () {
+
+    }
+
+
+}
---
+++ src/item.java
@@ -0,0 +1,7 @@
+public class item {
+
+    String name;
+    int weight;
+
+
+}
